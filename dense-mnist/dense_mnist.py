60e032f62856717e7335ed74dcd959855f7fa0d7
Hira Taqdees <hira.syeda@data61.csiro.au>
2024-04-16 13:38:00 +1000

    Update TF NN for MNIST

--- dense-mnist/dense_mnist.py
+++ dense-mnist/dense_mnist.py
@@ -1,16 +1,21 @@
 import sys
+import os
 import numpy as np
+import warnings
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 print("Loading MNIST dataset")
+
 (ds_train, ds_test), ds_info = tfds.load(
     'mnist',
     split=['train', 'test'],
@@ -72,7 +77,6 @@ val_acc = history['val_sparse_categorical_accuracy']
 loss = history['loss']
 val_loss = history['val_loss']
 
-
 epochs = range(1, len(acc) + 1)
 plt.subplot(2, 1, 1)
 plt.plot(epochs, acc, 'r', label='Training Accuracy')
@@ -86,7 +90,7 @@ plt.plot(epochs, loss, 'r', label='Training Loss')
 plt.plot(epochs, val_loss, 'b', label='Validation Loss')
 plt.title('Training and Validation Loss')
 plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
+plt.ylabel('Loss')
 
 plt.show()
 
@@ -95,27 +99,63 @@ model.save("model.keras")
 with open('model.summary', 'w') as sys.stdout:
     model.summary()
 
+# Extract weights and biases
+weights_and_biases = [
+    (layer.get_weights()[0], layer.get_weights()[1])
+    for layer in model.layers if len(layer.get_weights()) > 0]
+
+for i, (weights, biases) in enumerate(weights_and_biases):
+    np.savez(f"layer_{i}_weights_biases.npz", weights=weights, biases=biases)
+
+# Create a directory to save the files if it does not exist
+save_dir = 'model_weights_csv'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# Loop through each layer, extract weights and biases, and save them
+for i, (weights, biases) in enumerate(weights_and_biases):
+    np.savetxt(os.path.join(save_dir, f'layer_{i}_weights.csv'), weights, delimiter=',', fmt='%f')
+    np.savetxt(os.path.join(save_dir, f'layer_{i}_biases.csv'), biases, delimiter=',', fmt='%f')
+
 # ...reset 'sys.stdout' for later script output
 sys.stdout = sys.__stdout__
 
 print("Evaluating the testing dataset")
 
-result = model.evaluate(ds_test)
+model.evaluate(ds_test)
+print("Calculating Confusion Matrix")
+
+labels = []
+predicted_classes = []
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=ResourceWarning)
+
+for image_batch, label_batch in ds_test:
+    predictions = model.predict(image_batch)
+    predicted_labels = np.argmax(predictions, axis=1)
+    labels.extend(label_batch.numpy())
+    predicted_classes.extend(predicted_labels)
+
+# Calculate confusion matrix
+conf_matrix = confusion_matrix(labels, predicted_classes)
 print("Confusion Matrix")
-predictions = model.predict(ds_test)
-predicted_labels = np.argmax(predictions, axis=1)
+print(conf_matrix)
 
-ds = ds_test.take(1)
-test_labels = tfds.as_numpy(ds)
+try:
+    plt.figure(figsize=(10, 7))
+    plt.imshow(conf_matrix, cmap='viridis', interpolation='nearest')
+    plt.colorbar()
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.xticks(np.arange(10))
+    plt.yticks(np.arange(10))
+    plt.show()
+except KeyboardInterrupt:
+    print("Plotting was interrupted.")
+finally:
+    plt.close('all')
 
-'''
-# Compute the confusion matrix
-cm = confusion_matrix(test_labels, predicted_labels)
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=range(10), yticklabels=range(10))
-plt.xlabel('Predicted Labels')
-plt.ylabel('True Labels')
-plt.title('Confusion Matrix for MNIST Classification')
-plt.show()
-'''
-
+# At the end of your script
+K.clear_session()
